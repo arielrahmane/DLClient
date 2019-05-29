ea92da147fe010c73f533bc992f0ecc6971d461e
Ariel Rahmane <arielrahmane@gmail.com>
2019-05-29 17:06:07 -0300

    Irrelevant pages deleted

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,7 @@
 import Home from './pages/home.vue';
 import AboutPage from './pages/about.vue';
-import FormPage from './pages/form.vue';
-import DynamicRoutePage from './pages/dynamic-route.vue';
 
 import PanelLeftPage from './pages/panel-left.vue';
-import ColorThemes from './pages/color-themes.vue';
-import Chat from './pages/chat.vue';
-import Vuex from './pages/vuex.vue';
 
 export default [
   {
@@ -18,27 +13,7 @@ export default [
     component: AboutPage
   },
   {
-    path: '/form/',
-    component: FormPage
-  },
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage
-  },
-  {
     path: '/panel-left/',
     component: PanelLeftPage
-  },
-  {
-    path: '/color-themes/',
-    component: ColorThemes
-  },
-  {
-    path: '/chat/',
-    component: Chat
-  },
-  {
-    path: '/vuex/',
-    component: Vuex
-  },
+  }
 ];
